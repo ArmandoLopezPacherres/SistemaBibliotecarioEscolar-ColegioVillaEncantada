63e6c08460e9d6a7fe4ec600ab7ae86589b801c1
JeremyMartinezx <U22241773@utp.edu.pe>
2026-04-27 22:28:46 -0500

    chore: preparar clase runner para inicializacion de datos

---
+++ src/main/java/sistema_biblioteca/demo/config/DataInitializer.java
@@ -0,0 +1,37 @@
+package sistema_biblioteca.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import sistema_biblioteca.demo.model.Usuario;
+import sistema_biblioteca.demo.repository.UsuarioRepository;
+import sistema_biblioteca.demo.model.enums.RolUsuario;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+   
+    }
+
+    private void crearUsuarioSiNoExiste(String codigo, String nombre, RolUsuario rol) {
+        if (usuarioRepository.findByCodigo(codigo).isEmpty()) {
+            Usuario usuario = new Usuario();
+            usuario.setNombre(nombre);
+            usuario.setCodigo(codigo);
+            usuario.setPassword(passwordEncoder.encode("1234"));
+            usuario.setRol(rol);
+
+            usuarioRepository.save(usuario);
+            System.out.println("Usuario de prueba creado -> Código: " + codigo + " | Rol: " + rol.name());
+        }
+    }
+}
