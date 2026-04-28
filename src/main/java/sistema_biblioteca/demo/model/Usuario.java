29ae084601b498da2e8e6b88977a72320741b120
ArmandoLopezPacherres <U22305277@utp.edu.pe>
2026-04-27 21:45:25 -0500

    feat: modelar entidad Usuario para la gestion de cuentas

---
+++ src/main/java/sistema_biblioteca/demo/model/Usuario.java
@@ -0,0 +1,60 @@
+package sistema_biblioteca.demo.model;
+
+import jakarta.persistence.*;
+import sistema_biblioteca.demo.model.enums.RolUsuario;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String codigo;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RolUsuario rol;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RolUsuario getRol() {
+        return rol;
+    }
+
+    public void setRol(RolUsuario rol) {
+        this.rol = rol;
+    }
+}
