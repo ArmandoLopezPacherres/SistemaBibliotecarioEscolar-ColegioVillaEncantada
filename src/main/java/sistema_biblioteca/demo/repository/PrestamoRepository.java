cf3873fcdf333f51f9bd8db85eb20b8cdf6a11d1
Aguxitho <U22225372@utp.edu.pe>
2026-04-28 00:24:26 -0500

    fix(repository): agregar import faltante de List en PrestamoRepository

--- src/main/java/sistema_biblioteca/demo/repository/PrestamoRepository.java
+++ src/main/java/sistema_biblioteca/demo/repository/PrestamoRepository.java
@@ -2,6 +2,7 @@ package sistema_biblioteca.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sistema_biblioteca.demo.model.Prestamo;
+import java.util.List;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
 
