26fcb9f69328c5b7ddc0393a97cf44ee56c4d28a
JeremyMartinezx <U22241773@utp.edu.pe>
2026-05-31 02:05:45 -0500

    feat(repository): crear LibroRepository para consultas de inventario

---
+++ src/main/java/sistema_biblioteca/demo/repository/LibroRepository.java
@@ -0,0 +1,9 @@
+package sistema_biblioteca.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sistema_biblioteca.demo.model.Libro;
+import java.util.List;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    List<Libro> findByTituloContainingIgnoreCaseOrAutorNombreCompletoContainingIgnoreCase(String titulo, String autorNombre);
+}
